83e66bcd1227e1520bb0656721a15f9487f1954d
Patrick <xa.patjb4@gmail.com>
2019-10-26 02:38:36 -0400

    image receive code added

--- nodeapp/server.js
+++ nodeapp/server.js
@@ -1,10 +1,26 @@
 const express = require('express')
+var fs = require("fs");
+var bodyParser = require("body-parser");
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+//Take the image send it to google to identify the object then
+app.post('/image',(req,res) =>{
+  var name = req.body.name;
+  var img = req.body.image;
+  var realFile = Buffer.from(img,"base64");
+  fs.writeFile(name, realFile, function(err) {
+      if(err)
+         console.log(err);
+   });
+   res.send("OK");
+
+});
+
 app.listen(8000, () => {
   console.log('Example app listening on port 8000!')
+  
 });
