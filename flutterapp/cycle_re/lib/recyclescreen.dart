6eaa0f5cbf4dac3769244b3451fc745d75ed38be
Patrick <xa.patjb4@gmail.com>
2019-10-27 12:44:46 -0400

    final commit

--- flutterapp/cycle_re/lib/recyclescreen.dart
+++ flutterapp/cycle_re/lib/recyclescreen.dart
@@ -17,17 +17,17 @@ class _RecycleScreen extends State<RecycleScreen> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Result'),
+        title: new Text(widget.bin),
         backgroundColor: detCol(),
       ),
       body: Container(
         child:Column(
           children: <Widget>[
             new Container(
-              child: Text(prompt()),
+              child: new Text(prompt()),
             ),
             new Container(
-              child: Image.asset(img(), fit: BoxFit.cover),
+              child: new Image.asset(img(), fit: BoxFit.cover),
             )
           ],)
       ),
@@ -48,7 +48,7 @@ class _RecycleScreen extends State<RecycleScreen> {
         img = 'assets/images/metal.jpeg';
         break;
       case "paper":
-      case "carboard":
+      case "cardboard":
         img = 'assets/images/paper.jpg';
         break;
     }
@@ -69,7 +69,7 @@ class _RecycleScreen extends State<RecycleScreen> {
         color = Colors.lightGreen;
         break;
       case "paper":
-      case "carboard":
+      case "cardboard":
         color = Colors.lightBlue;
         break;
     }
@@ -86,20 +86,27 @@ class _RecycleScreen extends State<RecycleScreen> {
         + "* How do we stop trash?\n"
         + "- Buying recycled-content products and choosing products that have less packaging are also great ways to cut the amount of trash you generate";
         break;
-      case "plastic":
       case "metal":
+        prompt = " ---> Throw into plastic/metal/glass bin\n\n"
+        + "* Metal is just one of many materials we use daily in our lives and its really worth recycling when it has served its purpose.\n"
+        + "* The type of objects that can be recycled includes aluminum and steel cans, electrical appliances, silverware, office desks and chairs, old household pots and pans, school or sports lockers. safety boots or zippers, the list is endless\n"
+        + "* Metal recycling is good for the national economy and the environment e.g. increased local employment and reduced emissions.\n"
+        + "- Scrap metals are refashioned for use in creating bikes, cars, aircraft and other modes of transportation\n"
+        + "Our mission is to create a sustainable environment in the society. By using our app, you have helped us to move forward to a more sustainable society. Thank you!";
+        break;        
       case "glass":
+      case "plastic":
         prompt = " ---> Throw into plastic/metal/glass bin\n\n"
         + "* Plastics that can be recycled are first sorted, shredded and rid of impurities like paper.\n\n"
         + "* The shreds are then melted and formed into pellets, which can be made into other products.\n\n"
-        + "* How do we stop plastic?:\n"
+        + "* How do we stop using plastic?:\n"
         + "- The best solutions for our plastic problem are still to reduce our use of them, and to reuse and recycle whenever possible. More policies that ban plastic bag use,\n"
         + "- Require bottle deposits and expand recycling would help. But millions of tons of plastic waste still sit in landfills around the country\n"
         + "- Technologies that are able to tap this waste as a resource can offer multiple benefits, helping to clean up the environment, lessen our dependence on foreign oil,\n"
         + "- Decrease our use of non-renewable virgin resources, reduce greenhouse gas emissions, and generate energy";
         break;
       case "paper":
-      case "carboard":
+      case "cardboard":
         prompt = " ---> Throw into paper/cardboard bin\n\n"
         + "* Each year, over 400 million metric tons of paper and cardboard is produced globally, with over half of production originating from China, the United States, and Japan.\n\n"
         + "* Each metric ton (1,000 kilograms) of recycled paper can save approximately:\n"
